fe030d5eb0f77f1916b52fefa0b0245ac220e7da
maurizio-dominici <mauriziodominici4@gmail.com>
2025-05-20 15:56:47 +0200

    add structure

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/footer";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <header>
+      <div className="container">
+        <div className="col-1-2">
+          <img src="/dc-logo.png" alt="" />
+        </div>
+        <div className="col-1-2"></div>
+      </div>
+    </header>
+  );
+}
