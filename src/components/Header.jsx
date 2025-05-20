45aed46c36dacad30ce3dccfddcda070ff62d90a
maurizio-dominici <mauriziodominici4@gmail.com>
2025-05-20 16:31:21 +0200

    add header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,47 @@ export default function Header() {
   return (
     <header>
       <div className="container">
-        <div className="col-1-2">
-          <img src="/dc-logo.png" alt="" />
+        <div className="row">
+          <div className="col-1-5">
+            <img src="/dc-logo.png" alt="" />
+          </div>
+          <div className="col-1-3">
+            <nav>
+              <ul>
+                <li>
+                  <a href="#">charaters</a>
+                </li>
+                <li className="active">
+                  <a href="#">comics</a>
+                </li>
+                <li>
+                  <a href="#">moovie</a>
+                </li>
+                <li>
+                  <a href="#">tv</a>
+                </li>
+                <li>
+                  <a href="#">games</a>
+                </li>
+                <li>
+                  <a href="#">video</a>
+                </li>
+                <li>
+                  <a href="#">fans</a>
+                </li>
+                <li>
+                  <a href="#">news</a>
+                </li>
+                <li>
+                  <a href="#">shop</a>
+                </li>
+                <li>
+                  <a href="#">collectibles</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
         </div>
-        <div className="col-1-2"></div>
       </div>
     </header>
   );
